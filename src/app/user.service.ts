e416193b528d23f8d35fd7e950166d2da5c1917e
Dragutin <dragutin.avdalovic@walter.ba>
2019-05-08 09:56:01 +0200

    users service done

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
-
-
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { User} from './user';
+import { catchError, map, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor() { }
+  private usersUrl = 'https://jsonplaceholder.typicode.com/users';
+  constructor( private http: HttpClient) { }
+  /** GET heroes from the server */
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl);
+  }
 }
