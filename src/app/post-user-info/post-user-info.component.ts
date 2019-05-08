8314a6fed6660fb23ef257721a3e4a252e2bb94b
Dragutin <dragutin.avdalovic@walter.ba>
2019-05-08 15:59:36 +0200

    user info routing in progress

--- src/app/post-user-info/post-user-info.component.ts
+++ src/app/post-user-info/post-user-info.component.ts
@@ -4,6 +4,7 @@ import { User} from '../user';
 import { PostService} from '../post.service';
 import { UserService } from '../user.service';
 import {Router} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-user-info',
@@ -13,11 +14,14 @@ import {Router} from '@angular/router';
 export class PostUserInfoComponent implements OnInit {
   post: Post[];
   user: User[];
-  constructor(private postService: PostService, private userService: UserService, private router: Router) { }
+  postId: string;
+  userId: number;
+  constructor(private postService: PostService, private userService: UserService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.getPost('1');
-    this.getUser(1);
+    this.postId = this.route.snapshot.paramMap.get('id');
+    this.getPost(this.postId);
+    this.getUser(2);
   }
   getPost(id: string): void {
     this.postService.getPost(id)
