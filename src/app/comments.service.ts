9f3a1f340893a14fe8ee5680a8b213e23766933c
Dragutin <dragutin.avdalovic@walter.ba>
2019-05-13 15:42:41 +0200

    get commetn fn added

--- src/app/comments.service.ts
+++ src/app/comments.service.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Comment } from './comment';
 import { catchError, map, tap } from 'rxjs/operators';
+import {Post} from './post';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,8 @@ export class CommentsService {
   /** GET heroes from the server */
   getComments(): Observable<Comment[]> {
     return this.http.get<Comment[]>(this.commentsUrl);
+  }
+  getComment(id: string): Observable<Comment[]> {
+    return this.http.get<Comment[]>(this.commentsUrl + '/' + id);
   }
 }
