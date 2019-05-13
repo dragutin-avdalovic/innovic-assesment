58b5e4c098b428a6b2775c91ae0e918f66e95c28
Dragutin <dragutin.avdalovic@walter.ba>
2019-05-13 16:22:23 +0200

    comment service and html handle added

--- src/app/post-comments/post-comments.component.ts
+++ src/app/post-comments/post-comments.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Comment} from '../comment';
 import { CommentsService} from '../comments.service';
+import {Router} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-comments',
@@ -9,12 +11,20 @@ import { CommentsService} from '../comments.service';
 })
 export class PostCommentsComponent implements OnInit {
   comments: Comment[];
-  constructor(private commentsService: CommentsService) { }
+  comment: Comment[];
+  id: string;
+  constructor(private commentsService: CommentsService, private router: Router, private route: ActivatedRoute) { }
   ngOnInit() {
     this.getComments();
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.getComment(this.id);
   }
   getComments(): void {
     this.commentsService.getComments()
       .subscribe(comments => this.comments = comments);
+  }
+  getComment(id: string): void {
+    this.commentsService.getComment(id)
+      .subscribe(comment => this.comment = comment);
   }
 }
